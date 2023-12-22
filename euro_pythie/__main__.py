8c8afa318e778a9a8544743bb4d5a5fc6ff2f3fd
Hugo Porchet <hugo.porchet.belmonte@gmail.com>
2023-12-22 11:37:47 +0100

    feat: token in config.toml

--- euro_pythie/__main__.py
+++ euro_pythie/__main__.py
@@ -1,22 +1,30 @@
-import logging as log
 from interactions import Client, Intents, listen
+import logging as log
 import database
+import tomllib
 
-log.basicConfig(encoding="utf-8", format='%(asctime)s %(levelname)s: %(message)s', level=log.DEBUG)
+log.basicConfig(
+    encoding="utf-8", format="%(asctime)s %(levelname)s: %(message)s", level=log.DEBUG
+)
 log.info("Initilization of EuroPythie")
 
 bot = Client(
-        intents=Intents.DEFAULT,
-        sync_interactions=True,
-        asyncio_debug=True,
-        logger=log.getLogger("Interactions")
+    intents=Intents.DEFAULT,
+    sync_interactions=True,
+    asyncio_debug=True,
+    logger=log.getLogger("Interactions"),
 )
+
 
 @listen()
 async def on_ready():
     log.info("EuroPythie is ready")
 
+
 database.init()
 
+with open("config.toml", "rb") as c:
+    config = tomllib.load(c)
+
 bot.load_extension("game")
-bot.start("NjA5NzQ1MDAyNTkxMDI3MjIw.GtDQSO.CyLzHvLD4e2IjZ7qe5UfPiNiPiR8iO8K61_Nto")
+bot.start(config["token"])
