9b4cdd4b82b477512cf37db7369e9bae63b97211
Hugo Porchet <hugo.porchet.belmonte@gmail.com>
2024-03-18 14:05:37 +0100

    feat: ls crash with because of bad list conversion

--- euro_pythie/main.py
+++ euro_pythie/main.py
@@ -2,26 +2,25 @@ from interactions import Client, Intents, listen
 from .log import main as log
 from .config import bot as bot_conf
 import logging
-import euro_pythie.database as db
+from .database import database as db
 
 
-def init(): 
-    log.info("Initilization of EuroPythie")
+log.info("Initilization of EuroPythie")
 
-    bot = Client(
-        intents=Intents.DEFAULT,
-        sync_interactions=True,
-        asyncio_debug=True,
-        logger=logging.getLogger("Interactions"),
-    )
-
-
-    @listen()
-    async def on_ready():
-        log.info("EuroPythie is ready")
+bot = Client(
+    intents=Intents.DEFAULT,
+    sync_interactions=True,
+    asyncio_debug=True,
+    logger=logging.getLogger("Interactions"),
+)
 
 
-    db.init()
+@listen()
+async def on_ready():
+    log.info("EuroPythie is ready")
 
-    bot.load_extension("euro_pythie.game")
-    bot.start(bot_conf["token"])
+
+db.init()
+
+bot.load_extension("euro_pythie.game")
+bot.start(bot_conf["token"])
